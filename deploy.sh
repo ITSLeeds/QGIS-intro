b0dd8fbf6074f174cfd7483253716ea850d8c7f0
Robin Lovelace <rob00x@gmail.com>
2019-03-04 17:28:28 +0000

    Updates for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,20 @@
 # Aim: build and deploy book
 
+# in a shell
+git clone git@github.com:ITSLeeds/QGIS-intro
+cd QGIS-intro
+git checkout gh-pages
+cd ..
+mv -v _book/* QGIS-intro/
+
+# build book...
+cd QGIS-intro
+git status
+git add -A
+git commit -m 'Update book'
+git push
+cd ..
+
 # first time run:
 # git symbolic-ref HEAD refs/heads/gh-pages
 # rm .git/index
@@ -9,22 +24,22 @@
 # git commit -a -m "First pages commit"
 # git push origin gh-pages
 
-# push book contents:
-git commit -am 'pre-deploy commit'
-Rscript -e 'bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)'
-rm -rv /tmp/_book
-rm -rv /tmp/booksource
-mv _book /tmp/
-ls /tmp/_book
-git checkout gh-pages
-mkdir /tmp/booksource
-mv -v * /tmp/booksource/
-mv -v /tmp/_book/* .
-git add -A
-git commit -am 'Update book'
-git push origin gh-pages
-git checkout master
-mv -v /tmp/booksource/* .
-rm -rv *.html *.json libs
+# # push book contents:
+# git commit -am 'pre-deploy commit'
+# Rscript -e 'bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)'
+# rm -rv /tmp/_book
+# rm -rv /tmp/booksource
+# mv _book /tmp/
+# ls /tmp/_book
+# git checkout gh-pages
+# mkdir /tmp/booksource
+# mv -v * /tmp/booksource/
+# mv -v /tmp/_book/* .
+# git add -A
+# git commit -am 'Update book'
+# git push origin gh-pages
+# git checkout master
+# mv -v /tmp/booksource/* .
+# rm -rv *.html *.json libs
 
 
